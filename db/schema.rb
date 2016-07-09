1defa35feb9b1c8a55246679e2280a796f355a07
Benjamin Feight <bjfeight@gmail.com>
2016-07-09 07:50:21 -0400

    Finished Authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709025508) do
+ActiveRecord::Schema.define(version: 20160709105516) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160709025508) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "admin"
   end
 
 end
